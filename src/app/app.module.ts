eea2415c5980127d0481cfe3a32c40427c9d65ae
Ali Akbar <alibaloch3871@gmail.com>
2023-03-15 23:50:29 +0500

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentDataModuleModule } from './student-data-module/student-data-module.module';
-import {ToDoModulesModule} from './todo-module/todo-module.module'
+import {ToDoModulesModule} from './todo-module/todo-module.module';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
